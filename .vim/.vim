05d209a6199c184e4441d151d1c65690a13c0473
2k0ri <esc13245@gmail.com>
2014-03-25 12:21:47 +0900

    add .emacs*

---
+++ .vim/.vim
@@ -0,0 +1,1 @@
+/Users/K/dotfiles/.vim
--- setup.sh
+++ setup.sh
@@ -4,3 +4,5 @@ ln -s ~/dotfiles/oh-my-zsh_custom ~/.oh-my-zsh/custom
 ln -s ~/dotfiles/.vim ~/.vim
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/Brewfile ~/Brewfile
+ln -s ~/dotfiles/.emacs ~/.emacs
+ln -s ~/dotfiles/.emacs.d ~/.emacs.d

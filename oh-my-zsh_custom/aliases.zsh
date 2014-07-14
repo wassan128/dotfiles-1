6ffd75cff99bac549ca02e5d8328f81e46bbaf41
2k0ri <esc13245@gmail.com>
2014-07-15 01:50:28 +0900

    rmtrash追加

--- oh-my-zsh_custom/aliases.zsh
+++ oh-my-zsh_custom/aliases.zsh
@@ -1,5 +1,6 @@
 alias brew-cask-upgrade='for c in `brew cask list`; do ! brew cask info $c | grep -qF "Not installed" || brew cask install $c; done'
 alias rm='rm -i'
+if which rmtrash > /dev/null; then alias rm='rmtrash'; fi
 alias mv='mv -i'
 alias cp='cp -i'
 

79ff723de421a0f1ab201048cf77d33eba0fca19
2k0ri <esc13245@gmail.com>
2014-11-09 15:34:35 +0900

    console-capture修正

--- console-capture.sh
+++ console-capture.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
-_DATE=`date '+%Y%m%d%H%M%S'`
+_DATE=`date '+%y%m%d-%H:%M:%S'`
 
 tmux capture-pane -S -7000
 tmux show-buffer > /tmp/console-${_DATE}.log
-cat /tmp/console-${_DATE}.log | nkf -wLux | grep -P '❯ |\[.+?\][#$] ' | /usr/local/bin/sed -e '/```/d' -e '1i\```' -e '$a\```' | pbcopy
+cat /tmp/console-${_DATE}.log | nkf -wLux | grep -P '[\d:]{8}.+(↵ \$|\$ )|\[.+?\][#$] |^mysql.*>' | /usr/local/bin/sed -e '/```/d' -e '1i\```' -e '$a\```' | pbcopy
 tmux display-message "capture logged to /tmp/console-${_DATE}.log"

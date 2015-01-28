c6dbfc1d9fd2f7a27d5cd63cdd0ef10209377a94
2k0ri <esc13245@gmail.com>
2015-01-29 03:03:00 +0900

    自動補完追加、bind-Q微修正、sshec2改修

--- oh-my-zsh_custom/aws.zsh
+++ oh-my-zsh_custom/aws.zsh
@@ -1,4 +1,4 @@
 sshec2() {
-    [ $# -eq 0 ] && local _IP=`aws ec2 describe-instances | jq -r '.Reservations[].Instances[].NetworkInterfaces[].PrivateIpAddresses[].Association.PublicIp'` && echo $_IP
-    ssh -l ec2-user -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o LogLevel=FATAL -i ~/.ssh/AWS_Tokyo_K_OSX.pem ${1:-$_IP}
+    [[ $# == 0 ]] && local _IP="`aws ec2 describe-instances | jq -r '.Reservations[].Instances[].NetworkInterfaces[].PrivateIpAddresses[].Association.PublicIp' | sed '/null/d'`" && echo $_IP
+    ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o LogLevel=FATAL -i ~/.ssh/AWS_Tokyo_K_OSX.pem -l ${SSH_USER:-ec2-user} ${1:-$_IP}
 }
--- oh-my-zsh_custom/zsh-completion.zsh
+++ oh-my-zsh_custom/zsh-completion.zsh
@@ -1,3 +1,4 @@
 fpath=($(brew --prefix)/share/zsh/site-functions $fpath)
 autoload -Uz compinit
 compinit -C
+. /usr/local/share/zsh/site-functions/*

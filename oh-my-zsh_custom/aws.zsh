e11aa97978bd0a1fbbe4308867339096aad4867a
2k0ri <esc13245@gmail.com>
2015-02-02 11:34:05 +0900

    sshec2をコマンドレットに

--- oh-my-zsh_custom/aws.zsh
+++ oh-my-zsh_custom/aws.zsh
@@ -1,4 +1,12 @@
 sshec2() {
-    [[ $# == 0 ]] && local _IP="`aws ec2 describe-instances | jq -r '.Reservations[].Instances[].NetworkInterfaces[].PrivateIpAddresses[].Association.PublicIp' | sed '/null/d'`" && echo $_IP
-    ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o LogLevel=FATAL -i ~/.ssh/AWS_Tokyo_K_OSX.pem -l ${SSH_USER:-ec2-user} ${1:-$_IP}
+    while getopts :l OPT
+    do
+        case $OPT in
+            l)
+                aws ec2 describe-instances | jq -r '.Reservations[].Instances[] | {InstanceId, PrivateIpAddress, PublicAddress: (.NetworkInterfaces[].PrivateIpAddresses[].Association.PublicIp), InstanceName: (.Tags[] | select(.Key=="Name").Value)}'
+                return $?
+                ;;
+        esac
+    done
+    ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o LogLevel=FATAL -l ec2-user $*
 }

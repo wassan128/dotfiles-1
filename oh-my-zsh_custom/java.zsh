db669b47af19d49bd2ff868a31802223e5dd8f6c
2k0ri <esc13245@gmail.com>
2015-10-24 18:22:16 +0900

    java.zsh

---
+++ oh-my-zsh_custom/java.zsh
@@ -0,0 +1,3 @@
+export JAVA_HOME="$(/usr/libexec/java_home)"
+export CATALINA_HOME=/usr/local/Cellar/tomcat/libexec
+export CATALINA_OPTS="-Djava.library.path=/usr/local/Cellar/tomcat-native/1.1.33/lib"

33e9ee39d5b7106b473e17a146d3776a1e6ed7d9
Kei Kori <kori@aucfan.com>
2016-08-03 16:09:25 +0900

    add PHP_BUILD environments

--- oh-my-zsh_custom/anyenv.zsh
+++ oh-my-zsh_custom/anyenv.zsh
@@ -1,3 +1,7 @@
 export PATH="$HOME/.anyenv/bin:$PATH"
 eval "$(anyenv init - --no-rehash)"
+# pyenv
 eval "$(pyenv virtualenv-init - --no-rehash)" 2> /dev/null
+# phpenv
+export PHP_BUILD_CONFIGURE_OPTS="--with-openssl=/usr/local/opt/openssl --with-libxml-dir=/usr/local/opt/libxml2"
+export PHP_BUILD_EXTRA_MAKE_ARGUMENTS=-j4

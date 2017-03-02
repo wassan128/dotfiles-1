c1a58dc9451e7efcbb6c0192a89188742a74c173
2k0ri <esc13245@gmail.com>
2017-03-02 14:05:50 +0900

    update

--- zshrc.d/docker.zsh
+++ zshrc.d/docker.zsh
@@ -2,3 +2,5 @@ eval "$(timeout 1 docker-machine env default 2> /dev/null)"
 alias d='docker'
 dsh(){docker exec -it "${@:-$(docker ps -qla)}" bash;}
 alias dps='docker ps'
+alias dc=docker-compose
+alias dm=docker-machine
